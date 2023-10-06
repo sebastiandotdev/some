b6ed1ae94e0c02f4972915827578da59caa28a8f
johancs.mm@gmail.com <johancs.mm@gmail.com>
2023-10-05 22:21:25 -0500

    chore: 📌New site labs-company.org

---
+++ content/_index.md
@@ -0,0 +1,5 @@
+---
+banner: 
+    title: Bienvenido a Labs Company
+    content: Lema de labs company
+---
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./content/**/*.md",
+    "./layouts/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
