b59d8debb0eb01350ee48303ea692a55028b84cb
johancs.mm@gmail.com <johancs.mm@gmail.com>
2023-10-05 22:48:05 -0500

    chore: 📌 Add bash config with vercel

---
+++ vercel.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+
+# default versions
+
+NODE_VERSION='18.16.1'
+GO_VERSION='1.21.1'
+HUGO_VERSION='0.119.0'
+
+
+echo "USING NODE VERSION: $(node -v)"
+
+echo "Installing Go $GO_VERSION..."
