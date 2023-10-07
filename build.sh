7bd26a016b356b766b3d28fa746a89494ca725e8
johancs.mm@gmail.com <johancs.mm@gmail.com>
2023-10-06 22:56:54 -0500

    chore: 📌 Add config for vercel

--- build.sh
+++ build.sh
@@ -12,3 +12,21 @@ HUGO_VERSION='0.119.0'
 echo "USING NODE VERSION: $(node -v)"
 
 echo "Installing Go $GO_VERSION..."
+curl -sSOL https://dl.google.com/go/go${GO_VERSION}.linux-amd64.tar.gz
+tar -C /usr/local -xzf go${GO_VERSION}.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+rm -rf go${GO_VERSION}.linux-amd64.tar.gz
+go version
+
+echo "Installing Hugo $HUGO_VERSION..."
+curl -sSOL https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
+tar -xzf hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
+mv hugo /usr/local/bin/
+rm -rf hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
+hugo version
+
+echo "Installing project dependencies..."
+npm install
+
+echo "Running the build command..."
+npm run build
